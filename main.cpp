3e1150f372e9318987d3edf47a3777ec182261d8
Konsilerinos <n_slesarev@mail.ru>
2021-03-19 12:33:42 +0300

    Adjusted light beams

--- main.cpp
+++ main.cpp
@@ -1279,20 +1279,23 @@ int main() {
 	txCreateWindow(1800, 900);
 
 	parade::environment::CreateEnvironment();
-	parade::vehicles::CreateLightBeams(477, 325);
+
+	parade::vehicles::CreateLightBeams(916, 325);
+	parade::vehicles::CreateLightBeams(0, 325);
+	parade::vehicles::CreateLightBeams(1800, 325);
 
 	parade::buildings::BuildTower(916, 780);
 	parade::buildings::BuildMausoleum(901, 780);
 
 	parade::vehicles::AssembleAirDefenseSystem(491, 670);
 
-	parade::vehicles::AssebmleGAZAA(70, 820, parade::Camouflage::SUMMER);
- 	parade::vehicles::AssembleBM13(800, 820, parade::Camouflage::SUMMER);
-	parade::vehicles::AssembleBM13(1500, 820, parade::Camouflage::SUMMER);
+	parade::vehicles::AssebmleGAZAA(70, 820, parade::Camouflage::WINTER);
+ 	parade::vehicles::AssembleBM13(800, 820, parade::Camouflage::WINTER);
+	parade::vehicles::AssembleBM13(1500, 820, parade::Camouflage::WINTER);
 
-	parade::vehicles::AssembleT34(200, 890, parade::Camouflage::SUMMER);
-	parade::vehicles::AssembleBA3(850, 890, parade::Camouflage::SUMMER);
-	parade::vehicles::AssembleT34(1550, 890, parade::Camouflage::SUMMER);
+	parade::vehicles::AssembleT34(200, 890, parade::Camouflage::WINTER);
+	parade::vehicles::AssembleBA3(850, 890, parade::Camouflage::WINTER);
+	parade::vehicles::AssembleT34(1550, 890, parade::Camouflage::WINTER);
 
 	parade::aircraft::AssembleIL2(1000, 200);
 	parade::aircraft::AssembleIL2(100, 200);
